572961aebc3da30397060e14d297b0b8dad9b44d
zhangwinwin <344879340@qq.com>
2020-12-13 10:55:25 +0800

    feat: 增加Home组件的单元测试

--- unitest/src/views/Home.vue
+++ unitest/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Welcome to Your Vue.js App</p>
   </div>
 </template>
 
---
+++ unitest/tests/unit/Home.spec.js
@@ -0,0 +1,28 @@
+// import Vue from 'vue';
+// import Home from '@/views/Home.vue';
+
+// describe('Home.vue', () => {
+//   it('renders msg when mounted', () => {
+//     const msg = 'Welcome to Your Vue.js App';
+//     // 使用Home选项创建一个新的Vue构造函数
+//     const Ctor = Vue.extend(Home);
+//     // 创建一个新的Vue实例并挂载该实例
+//     const vm = new Ctor().$mount();
+//     // 访问DOM元素，检查文本内容
+//     expect(vm.$el.textContent).toContain(msg)
+//   })
+// })
+
+import { mount, shallowMount } from '@vue/test-utils';
+import Home from '@/views/Home.vue';
+
+describe('Home.vue', () => {
+  it('renders msg when mounted', () => {
+    const msg = 'Welcome to Your Vue.js App';
+    // 使用shallowMount方法挂载组件
+    // const wrapper = mount(Home);
+    const wrapper = shallowMount(Home);
+    // 检查文本内容
+    expect(wrapper.text()).toContain(msg)
+  })
+})
